07abd52a962df9c5fb25eda538dcdf13b0fa3666
Anh Vuong <vta19797@gmail.com>
2022-05-07 18:11:14 -0500

    build the database call

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -16,6 +17,31 @@ app.get('/', (req, res) => {
 app.use((req, res) => {
     res.status(404).end();
   });
+
+// Connect to database
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: '',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+  );
+
+  db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+  });
+
+
+
+
+
+
+
+
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
